bd8e866b6162711a710b866f73d5991b0489a864
Jerry <jerryappleid761208@gmail.com>
2020-09-14 14:37:53 -0700

    init

---
+++ apps/frontend/components/Batter.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+interface StyleProps {
+  level: number;
+}
+
+const BatteryStyles = styled.div<StyleProps>`
+  width: 100%;
+  --color: ${(props) => (props.level > 20 ? '#1af21a' : '#bb0707')};
+  border: 2px solid black;
+  border-radius: 5px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column-reverse;
+  background: #c5c5c5;
+
+  .batteryLevel {
+    transition: all 0.5s;
+    height: ${(props) => props.level};
+    text-align: center;
+    color: white;
+    display: block;
+    background: var(--color);
+  }
+`;
+
+interface IProps {
+  battery: number;
+}
+
+const Batter: React.FC<IProps> = ({ battery = 60 }) => (
+  <BatteryStyles level={battery}>
+    <span className="batteryLevel">{battery}%</span>
+  </BatteryStyles>
+);
+
+export default Battery;
